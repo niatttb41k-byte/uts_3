d5de7c8225c38b02d917fd06658a3abe87a29f57
sania <niatttbaik@gmail.com>
2026-01-01 15:10:01 +0800

    setoran tugas uts3

---
+++ utils1.py
@@ -0,0 +1,10 @@
+def parse_list(data):
+    return list(map(int, data.split()))
+
+
+def menghitung_nilai_terbesar(data):
+    return max(data)
+
+
+def menghitung_nilai_terkecil(data):
+    return min(data)
